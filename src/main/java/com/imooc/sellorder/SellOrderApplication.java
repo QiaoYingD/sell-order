257177aa2b1352478ce040834e11d57c30b1ae27
乔应东@qiayingdong.pn <qiaoyingdong.pn@rdunicloud.com>
2019-08-17 15:02:26 +0800

    搭建 sell-order 服务

---
+++ src/main/java/com/imooc/sellorder/SellOrderApplication.java
@@ -0,0 +1,18 @@
+package com.imooc.sellorder;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan(basePackages = {"com.imooc.sellorder.mapper"})
+public class SellOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/sellorder/SellOrderApplicationTests.java
@@ -0,0 +1,10 @@
+package com.imooc.sellorder;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellOrderApplicationTests {
+
+
+}
